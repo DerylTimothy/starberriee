96cd7cdc110826a29cdf1bb27d49381ff5ff5494
Deryl Timothy <email_github_lu@gmail.com>
2026-06-12 12:11:44 +0700

    auto-create sqlite file on boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Auto-create SQLite database file jika belum ada
+        if (config('database.default') === 'sqlite') {
+            $dbPath = config('database.connections.sqlite.database');
+            if ($dbPath && !file_exists($dbPath)) {
+                $dir = dirname($dbPath);
+                if (!is_dir($dir)) {
+                    mkdir($dir, 0755, true);
+                }
+                touch($dbPath);
+            }
+        }
     }
 }
