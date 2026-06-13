c6c6cf6a5e66c441fef4987032d78da6837cbbde
Deryl Timothy <email_github_lu@gmail.com>
2026-06-13 12:42:31 +0700

    debug: check product data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,12 +7,24 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    /**
+     * Menampilkan daftar semua produk untuk halaman Shop.
+     */
     public function index()
     {
-        // Mengambil semua data produk dari database
+        // Mengambil seluruh data produk dari tabel 'products'
         $products = Product::all();
 
-        // Mengirim data $products ke view 'shop'
+        // Mengirim data ke view 'shop.blade.php'
         return view('shop', compact('products'));
+    }
+
+    /**
+     * Menampilkan detail satu produk (opsional, jika nanti kamu butuh halaman detail).
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('shop-detail', compact('product'));
     }
 }
